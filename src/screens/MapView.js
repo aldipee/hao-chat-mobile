c8528eed8a5d568752079283963da085a716aa7b
aldipee <aldipeee@gmail.com>
2020-04-23 05:03:50 +0700

    add mapview

--- src/screens/MapView.js
+++ src/screens/MapView.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+// import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 import Geolocation from '@react-native-community/geolocation';
 Geolocation.setRNConfiguration({skipPermissionRequests: true});
 
-export default class MapView extends Component {
+export default class MapViews extends Component {
   componentDidMount() {
     Geolocation.getCurrentPosition(data => {
-      console.log('here', data);
+      this.setState();
+      console.log(data.coords);
     });
   }
   render() {
+    // {"accuracy": 26.87700080871582, "altitude": 0, "heading": 0, "latitude": -6.6211293, "longitude": 106.8180347, "speed": 0}
     return (
       <View>
         <Text>Hola</Text>
+        <View>{/* <MapView style={{flex: 1}} showUserLocation /> */}</View>
       </View>
     );
   }
